dcc08cf18e0be4a7c71c6db0ca111d5ca3d50a85
atinydev <atinydeveloper@gmail.com>
2022-03-04 13:32:55 -0600

    article design widget

--- lib/widgets/news_list.dart
+++ lib/widgets/news_list.dart
@@ -15,10 +15,112 @@ class NewsList extends StatelessWidget {
       itemCount: news.length,
       itemBuilder: (context, index) {
         final article = news[index];
-        return ListTile(
-          title: Text(article.title),
+        return _ArticleW(
+          article: article,
+          index: index,
         );
       },
     );
+  }
+}
+
+class _ArticleW extends StatelessWidget {
+  const _ArticleW({
+    Key? key,
+    required this.article,
+    required this.index,
+  }) : super(key: key);
+
+  final Article article;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        _TopBarCard(
+          article: article,
+          index: index,
+        ),
+        _TitleCard(
+          article: article,
+        ),
+        _ImageCard(
+          urlToImage: article.urlToImage ?? "",
+        )
+      ],
+    );
+  }
+}
+
+class _TopBarCard extends StatelessWidget {
+  const _TopBarCard({
+    Key? key,
+    required this.article,
+    required this.index,
+  }) : super(key: key);
+
+  final Article article;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      margin: const EdgeInsets.only(bottom: 10),
+      child: Row(
+        children: [
+          Text(
+            '${index + 1}',
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+          ),
+          Text(
+            article.source.name,
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _TitleCard extends StatelessWidget {
+  const _TitleCard({
+    Key? key,
+    required this.article,
+  }) : super(key: key);
+
+  final Article article;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Text(
+        article.title,
+        style: const TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.w700,
+        ),
+      ),
+    );
+  }
+}
+
+class _ImageCard extends StatelessWidget {
+  const _ImageCard({
+    Key? key,
+    required this.urlToImage,
+  }) : super(key: key);
+
+  final String urlToImage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
