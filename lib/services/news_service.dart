6132a05f4f1d60eb75a0aa584561f7be925f39b0
atinydev <atinydeveloper@gmail.com>
2022-03-04 12:58:37 -0600

    Headlines and .env

--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import 'package:http/http.dart' as http;
+import 'package:newsapp/models/models.dart';
 
 class NewsService extends ChangeNotifier {
   final headlines = [];
+  final _authority = 'newsapi.org';
+  final _unencodedPath = 'v2/top-headlines';
+  final _apiKey = dotenv.env['apiKey'];
 
   NewsService() {
     getTopHeadlines();
   }
 
-  Future<void> getTopHeadlines() async {}
+  Future<void> getTopHeadlines() async {
+    final url = Uri.https(_authority, _unencodedPath, {
+      'country': 'us',
+      'apiKey': _apiKey,
+    });
+    final resp = await http.get(url);
+    final newsResponse = NewsResponse.fromJson(resp.body);
+    headlines.addAll(newsResponse.articles);
+    notifyListeners();
+  }
 }
